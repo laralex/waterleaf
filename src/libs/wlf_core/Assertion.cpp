6676a638a6cd4c234f6f368d61803d6efd39d639
Alex Larionov <alxs.larionov@gmail.com>
2021-07-28 23:10:12 +0300

    Remove logging header (going to use raw spdlog, without facade)

--- src/libs/wlf_core/Assertion.cpp
+++ src/libs/wlf_core/Assertion.cpp
@@ -6,6 +6,8 @@
 #include <iostream>
 #include <string_view>
 
+
+
 using namespace wlf;
 
 namespace {
@@ -14,28 +16,26 @@ constexpr auto operator&(AssertionLevel lhs, AssertionLevel rhs) -> bool {
    return static_cast<ptrdiff_t>(lhs) & static_cast<ptrdiff_t>(rhs);
 }
 
+constexpr void AbortIfNotOk(bool isOk, std::string_view message) {
+   if(!isOk) {
+      spdlog::critical("Assertion failed: {}", message);
+      // TODO(laralex): fancier termination (error window, etc)
+      exit(1);
+   }
 }
+
+} // namespace
 
 /* Assertion utilities */
 
 void wlf::Assert(bool isOk, std::string_view message) {
    constexpr bool chosen = (EnabledAssertions & AssertionLevel::RunTime);
-   if constexpr(!wlf::NoAsserts && chosen) {
-      if(!isOk) {
-         spdlog::error(message);
-         // TODO(laralex): fancier termination (error window, etc)
-         exit(1);
-      }
-   }
+   if constexpr(!wlf::NoAsserts && chosen) { AbortIfNotOk(isOk, message); }
 }
 
 void wlf::AssertDebug(bool isOk, std::string_view message) {
    constexpr bool chosen = (EnabledAssertions & AssertionLevel::DebugRunTime);
    if constexpr(!wlf::NoAsserts && wlf::IsDebugBuild && chosen) {
-      if(!isOk) {
-         spdlog::error(message);
-         // TODO(laralex): fancier termination (error window, etc)
-         exit(1);
-      }
+      AbortIfNotOk(isOk, message);
    }
 }
